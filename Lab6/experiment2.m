da39a6f53e6b7d8e43e53e70c96713c609e6850c
li <lisa.hachmann@gmail.com>
2016-03-25 18:39:30 -0400

    Confused in Exp 1, started Exp2

--- Lab6/experiment2.m
+++ Lab6/experiment2.m
@@ -4,4 +4,11 @@ load exp2I_Vds10mV.csv
 load exp2I_VdsVdd.csv
 load exp2Vg_Vds10mV.csv
 load exp2Vg_VdsVdd.csv
+load exp2Parallel_VdsVdd.csv
+load exp2ParallelVg_Vds10mV.csv
+load exp2ParallelVg_VdsVdd.csv
+load exp2Vg_Vds10mV.csv
+load exp2Vg_VdsVdd.csv
 
+% a single semilog plot showing data from the individual transistor, the parallel 
+%connection, and the series connection, for each of the two experimental situations
