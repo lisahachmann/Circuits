f3f9639d6f44a2309d4984e20a5ce746dfe42f35
li <lisa.hachmann@gmail.com>
2016-03-28 10:01:04 -0400

    Did Vds = Vdd graph

--- Lab6/experiment2.m
+++ Lab6/experiment2.m
@@ -4,11 +4,28 @@ load exp2I_Vds10mV.csv
 load exp2I_VdsVdd.csv
 load exp2Vg_Vds10mV.csv
 load exp2Vg_VdsVdd.csv
-load exp2Parallel_VdsVdd.csv
+load exp2ParallelI_Vds10mV.csv
+load exp2ParallelI_VdsVdd.csv
 load exp2ParallelVg_Vds10mV.csv
 load exp2ParallelVg_VdsVdd.csv
+load exp2Vg_Vds10mV.csv
+load exp2Vg_VdsVdd.csv
+load exp2SeriesI_Vds10mV.csv
+load exp2SeriesI_VdsVdd.csv
+load exp2SeriesVg_Vds10mV.csv
+load exp2SeriesVg_VdsVdd.csv
 load exp2Vg_Vds10mV.csv
 load exp2Vg_VdsVdd.csv
 
 % a single semilog plot showing data from the individual transistor, the parallel 
 %connection, and the series connection, for each of the two experimental situations
+
+semilogy(abs(exp2Vg_VdsVdd), abs(exp2I_VdsVdd), 'b*')
+hold on
+semilogy(abs(exp2ParallelVg_VdsVdd), abs(exp2ParallelI_VdsVdd), 'ro')
+hold on
+semilogy(abs(exp2SeriesVg_VdsVdd), abs(exp2SeriesI_VdsVdd), 'm>')
+legend('Single Transistor', 'Parallel circuitry', 'Series circuitry')
+title('I-V characterisitcs at Vds = Vdd (5V)')
+xlabel('Gate voltage (V)')
+ylabel('Channel current (A)')
