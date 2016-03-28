3060beb2e2ca65255582eead47dd278f0f18b6b0
li <lisa.hachmann@gmail.com>
2016-03-28 10:34:23 -0400

    All experiment 3 graphs

--- Lab6/experiment3.m
+++ Lab6/experiment3.m
@@ -1,1 +1,27 @@
 %% Experiment 3
+
+load exp3Iina.csv
+load exp3Iinb.csv
+load exp3Ioutb.csv
+load exp3Iouta.csv
+
+% loglog(abs(exp3Iina), abs(exp3Iouta), 'ro')
+% P2 = polyfit(abs(exp3Iina(110:200)),abs(exp3Iouta(110:200)),1);
+% y2fit = 0.5077*abs(exp3Iina) + 10e-10 ;
+% hold on;
+% loglog(abs(exp3Iina),y2fit,'k', 'LineWidth', 2);
+% xlabel('Input current (Iin) (A)')
+% ylabel('Output current of 1 branch (I1) (A)')
+% title('Current divider, with slope = 0.5077')
+% legend('Measured data','Theoretical linear fit')
+
+%Circuit b
+loglog(abs(exp3Iinb), abs(exp3Ioutb), 'ro')
+P = polyfit(abs(exp3Iinb(110:200)),abs(exp3Ioutb(110:200)),1);
+yfit = P(1)*abs(exp3Iinb(110:200))+P(2);
+hold on;
+loglog(abs(exp3Iinb(110:200)),yfit,'k', 'LineWidth', 2);
+xlabel('Input current (Iin) (A)')
+ylabel('Output current of 1 branch (I1) (A)')
+title('Current divider, with slope = 0.4938')
+legend('Measured data','Theoretical linear fit')
