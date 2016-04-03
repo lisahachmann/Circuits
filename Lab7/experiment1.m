e5cd74bd95022fa3ac82a86152b9a58354a28c5c
li <lisa.hachmann@gmail.com>
2016-04-03 16:51:56 -0400

    Second plot ready, I think

--- Lab7/experiment1.m
+++ Lab7/experiment1.m
@@ -23,7 +23,7 @@ load V_V225Vb2.csv
 %include a single plot showing I1, I2, I1 − I2, and I1 + I2, as a function of V1 − V2 for 
 %all three values of V2 that you used. 
 
-
+figure 
 semilogy(V1V2diff,  I1_V220, 'ro', 'Markersize', 12)
 hold on
 semilogy(V1V2diff, I2_V220, 'bo', 'Markersize', 12)
@@ -54,7 +54,15 @@ ylabel('Difference in current between I1 and I2 (Amps)', 'FontSize', 13)
 %Also include a plot showing the common-source node voltage,
 %V , as a function of V1 − V2 for all three values of V2. 
 
-
+figure
+plot(V1V2diff, V_V215, 'mo')
+hold on
+plot(V1V2diff, V_V220, 'ro')
+hold on
+plot(V1V2diff, V_V225, 'bo')
+xlabel('Difference in V1 and V2 (V)', 'FontSize', 13)
+ylabel('Voltage at node V (V)', 'FontSize', 13)
+legend('V2 at 1.5V', 'V2 at 2V', 'V2 at 2.5V', 'FontSize', 11)
 
 %For each of the three values of V2 that you used, fit a straight line to the plot of 
 %I1 −I2 as a function of V1 − V2 around the region where V1 ≈ V2 (i.e., 
