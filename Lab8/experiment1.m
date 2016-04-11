f97e2abeca23fb68cbf7a57846d3d259b9ba6760
li <lisa.hachmann@gmail.com>
2016-04-11 11:00:21 -0400

    Figured out common mode input gain

--- Lab8/experiment1.m
+++ Lab8/experiment1.m
@@ -10,12 +10,18 @@ load exp1Vout_V235Vb15.csv
 load exp1Vout_V245.csv
 load exp1Vout_V245Vb15.csv
 
+P3 = polyfit(exp1V1Sweep(149:152),exp1Vout_V225(149:152),1);
+yfit3 = P3(1)*exp1V1Sweep(149:152)+P3(2);
+limits = [0, 5, 0, 5.25];
 figure
+plot(exp1V1Sweep(149:152),yfit3, 'm', 'Linewidth', 4)
+hold on
 plot(exp1V1Sweep, exp1Vout_V225, 'bo-')
 hold on
 plot(exp1V1Sweep, exp1Vout_V235, 'ro-')
 hold on
 plot(exp1V1Sweep, exp1Vout_V245, 'ko-')
+axis(limits)
 xlabel('V1 voltage sweep (V)')
 ylabel('Output voltage (V)')
 title('VTC with base voltage at threshold')
