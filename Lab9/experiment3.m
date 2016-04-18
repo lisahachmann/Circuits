52d63adf941450627ad4f6d8098fab1bd5ccbad1
li <lisa.hachmann@gmail.com>
2016-04-18 10:02:49 -0400

    Starting experiment 3

---
+++ Lab9/experiment3.m
@@ -0,0 +1,26 @@
+%%Lab 9 
+%experiment 3
+
+%load
+
+% Apply a small-amplitude square wave to the input of your circuit and observe both the
+%input and output waveforms as a function of time.
+%Extract a time constant both for the up-going and for the down-going
+%output transitions. How do these compare with that which you compute from the measured
+%values of the load capacitance and the differential-mode transconductance gain that you
+%found in Experiment 2? In your report, include a single plot showing both scope traces
+%along with the extracted time constants.
+
+
+
+
+%Next, increase the amplitude of your square wave so that it is a couple of volts.
+%You will probably notice that the output
+%of the amplifier follows a linear trajectory in time over most of its response 
+%to the large input step. This behavior is called slewing, and the constant rate
+%of change of the output voltage with respect to time is called the slew rate 
+%of the amplifier. Extract a slew rate for both for the up-going and for the down-going 
+%output transitions. How do these compare with those which you compute from the load 
+%capacitance and the limiting values of the output current?
+%In your report, include a single plot showing both scope traces along with 
+%the extracted slew rates.
