712b395bcd12e8db9a13e143f786ece855f356b7
li <lisa.hachmann@gmail.com>
2016-04-19 17:48:41 -0400

    Slew rate figure finished

--- Lab9/experiment3.m
+++ Lab9/experiment3.m
@@ -1,7 +1,9 @@
 %%Lab 9 
 %experiment 3
 
-%load
+load Large5kHz2V.csv
+load SmallWave_Zoomed.csv
+load SmallWave_ZoomedOut.csv
 
 % Apply a small-amplitude square wave to the input of your circuit and observe both the
 %input and output waveforms as a function of time.
@@ -11,8 +13,21 @@
 %found in Experiment 2? In your report, include a single plot showing both scope traces
 %along with the extracted time constants.
 
+t = SmallWave_Zoomed(:,1);
+tcut= t(3100:6000);
+Vout = SmallWave_Zoomed(:,3);
+Voutcut = Vout(3100:6000);
+Vin = SmallWave_Zoomed(:,2);
+Vincut = Vin(3100:6000);
 
-
+% figure 
+% limits = [0.5*10^(-4), 1.5*10^(-4), 0, 2.6];
+% axis(limits)
+% plot(tcut, Voutcut)
+% hold on
+% Tau = 0.0072;
+% y1 = Vincut.*(1 - exp(-tcut./Tau));
+% plot(tcut, y1, 'k')
 
 %Next, increase the amplitude of your square wave so that it is a couple of volts.
 %You will probably notice that the output
@@ -24,3 +39,21 @@
 %capacitance and the limiting values of the output current?
 %In your report, include a single plot showing both scope traces along with 
 %the extracted slew rates.
+% 
+
+tl = Large5kHz2V(:,1)
+tlcut = tl(1200:2600);
+sigl = Large5kHz2V(:,3);
+siglcut = sigl(1200:2600);
+
+P4 = polyfit(tlcut,siglcut,1);
+yfit4 = P4(1)*tlcut+P4(2);
+
+figure
+plot(Large5kHz2V(:,1), Large5kHz2V(:,2), 'r')
+hold on
+plot(Large5kHz2V(:,1), Large5kHz2V(:,3), 'b')
+plot(tlcut, yfit4, 'k', 'linewidth', 2)
+xlabel('Time (sec)')
+ylabel('Voltage (V)')
+legend('Input signal', 'Output signal', 'Best fit line')
