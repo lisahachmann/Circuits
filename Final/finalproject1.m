b3cf1167c8a6a289d99a29832b08ffe1ba1a7c11
li <lisa.hachmann@gmail.com>
2016-04-25 12:58:55 -0400

    Delay plot for SR latch 1

--- Final/finalproject1.m
+++ Final/finalproject1.m
@@ -1,2 +1,13 @@
-import delay1.csv
+load delay1.csv
 
+time = delay1(:,1);
+C1 = delay1(:,2);
+C2 = delay1(:,3);
+x = 6565:6570;
+
+plot(time(x), C1(x))
+hold on
+plot(time(x), C2(x))
+xlabel('Time (seconds)')
+ylabel('Voltage (V)')
+title('Time delay of simple SR latch circuit')
